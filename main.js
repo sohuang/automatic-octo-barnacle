86d2f0d51124a74dc3d9a9a6b2dfefca6e9f2072
Sophie <sophiexhuang@gmail.com>
2016-06-26 15:31:15 +0000

    not sure

--- main.js
+++ main.js
@@ -1,18 +1,40 @@
+// function checkChecked() {
+//     if (document.getElementById("great").checked) {
+//         alert("checked");
+//     } else {
+//         alert("not checked");
+//     }
+// }
+// 
 function helpMe() {
-    var checkboxes = document.querySelectorAll("input[type=checkbox]");
-    var checkboxesLength = checkboxes.length;
-    var checkboxesChecked;
-    for (var i = 0; i < checkboxesLength; i++) {
-        if (checkboxes[i].checked == true) {
-            checkboxesChecked.push(checkboxes[i].id);
-        }
+    // var checkboxes = document.querySelectorAll("div.checkbox");
+    // var arrayLength = checkboxes.length;
+    // var checkboxesChecked;
+    // for (var i = 0; i < arrayLength; i++) {
+    //     if (checkboxes[i].checked == true) {
+    //         checkboxesChecked.push(checkboxes[i].id);
+    //     }
+    // }
+    // 
+    if (document.getElementById("great").checked) {
+        document.getElementById("somethingGreat").style.display = "block";
+    } 
+    
+    if (document.getElementById("sad").checked) {
+        document.getElementById("somethingSad").style.display = "block";
     }
     
-    document.getElementById("feelGreatTitle").style.display = "block";
-    document.getElementById("feelGreatContent").style.display = "block";
-    document.getElementById("weatherWidget").style.display = "block";
+    if (document.getElementById("busy").checked) {
+        document.getElementById("somethingBusy").style.display = "block";
+    }
+    
+    if (document.getElementById("bored").checked) {
+        document.getElementById("somethingBored").style.display = "block";
+    }
     
     document.getElementById("siteTitle").style.display = "none";
-    document.getElementById("checkboxesFeelings").style.display = "none";
+    document.getElementById("radioFeelings").style.display = "none";
     document.getElementById("helpMeButton").style.display = "none";
+    
+    document.getElementById("weatherWidget").style.display = "block";
 }
